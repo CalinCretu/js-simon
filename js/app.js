486747da2cbae98d19909be0a537ccc9354dc492
CalinCretu <calincretu1412@gmail.com>
2023-10-18 10:10:10 +0200

    ADD - userNumbers_array

--- js/app.js
+++ js/app.js
@@ -6,13 +6,15 @@ document.querySelector(".random-numbers").innerHTML = randomNumbers.join(" ");
 timer = setTimeout(function () {
   // Nasconde i numeri casuali
   document.querySelector(".random-numbers").innerHTML = "";
-}, 30000);
-timer = setTimeout(userGuess, 30500)
+}, 5000);
+timer = setTimeout(userGuess, 5500)
+numeriUtente = []
 function userGuess() {
   let guessedNumbers = 0;
   for (let i = 0; i < randomNumbers.length; i++) {
-    let userNumber = prompt("Inserisci un numero:");
-    if (randomNumbers[i] == userNumber[i]) {
+    let userNumber = parseInt(prompt("Inserisci un numero:"));
+    numeriUtente.push(userNumber);
+    if (randomNumbers[i] == numeriUtente[i]) {
       guessedNumbers++;
     }
   }
